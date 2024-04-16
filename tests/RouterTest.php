08a556d49f09704af2bb8032461f9a472bf16bdf
ddesioeleva <ddesio@eleva.it>
2024-04-16 08:37:48 +0200

    Add more tests

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -73,6 +73,86 @@ class RouterTest extends TestCase
         self::assertSame('api.php', $router->match($this->request('PUT', '/tests/1'))[0]);
     }
 
+    /**
+     * @test
+     */
+    public function test_routing_with_api_gateway_v1_wildcard_config(): void
+    {
+        $config = <<<YAML
+        functions:
+          api:
+            handler: api.php
+            events:
+              - http:
+                  method: '*'
+                  path: '*'
+        YAML;
+
+        $config = Yaml::parse($config);
+        $router = Router::fromServerlessConfig($config);
+
+        self::assertSame('api.php', $router->match($this->request('GET', '/'))[0]);
+        self::assertSame('api.php', $router->match($this->request('POST', '/'))[0]);
+        self::assertSame('api.php', $router->match($this->request('GET', '/tests/1'))[0]);
+        self::assertSame('api.php', $router->match($this->request('PUT', '/tests/1'))[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_routing_with_api_gateway_v1_specific_method_config(): void
+    {
+        $config = <<<YAML
+        functions:
+          hello:
+            handler: hello.php
+            events:
+              - http:
+                  method: 'GET'
+                  path: '/hello'
+        YAML;
+
+        $config = Yaml::parse($config);
+        $router = Router::fromServerlessConfig($config);
+
+        self::assertSame('hello.php', $router->match($this->request('GET', '/hello'))[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_routing_with_api_gateway_v1_file_config(): void
+    {
+        $config = <<<YAML
+        functions:
+          - \${file(./tests/function/hello/v1serverless.yml)}
+        YAML;
+
+        $config = Yaml::parse($config);
+        $router = Router::fromServerlessConfig($config);
+
+        self::assertSame('hello.php', $router->match($this->request('GET', '/hello'))[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_routing_with_api_gateway_v2_file_config(): void
+    {
+        $config = <<<YAML
+        functions:
+          - \${file(./tests/function/hello/v2serverless.yml)}
+        YAML;
+
+        $config = Yaml::parse($config);
+        $router = Router::fromServerlessConfig($config);
+
+        self::assertSame('hello.php', $router->match($this->request('GET', '/'))[0]);
+        self::assertSame('hello.php', $router->match($this->request('POST', '/'))[0]);
+        self::assertSame('hello.php', $router->match($this->request('GET', '/tests/1'))[0]);
+        self::assertSame('hello.php', $router->match($this->request('PUT', '/tests/1'))[0]);
+    }
+
     private function request(string $method, string $path): ServerRequestInterface
     {
         return new ServerRequest($method, $path);
